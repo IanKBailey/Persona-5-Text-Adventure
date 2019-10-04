cfc547a2abe230ed16d29696ee4d51784d020b23
Ian Bailey <iankbailey2@gmail.com>
2019-10-04 15:58:02 -0400

    ending fixed

--- Text Adventure/main.swift
+++ Text Adventure/main.swift
@@ -27,7 +27,7 @@ var itemCount = 6
 var rakundaCount = 0
 var joker = (hattack: 50.0, hdefense: 50.0, health: 3000.0, sp: 50.0)
 var pixie = (e1attack: 60.0, e1defense: 100.0, health: 100.0)
-var archAngel = (e2attack: 70.0, e2defense: 200.0, health: 100.0)
+var archAngel = (e2attack: 70.0, e2defense: 200.0, health: 200.0)
 var itemHeal = Double((60...100).randomElement() ?? 1)
 
 func items () {
@@ -39,21 +39,28 @@ func items () {
 func eiha () {
     joker.sp -= 4
     pixie.health -= 100
+    archAngel.health -= 200
 }
 
 
 func attack () {
     pixie.health -= ((joker.hattack) * (100/(100+pixie.e1defense))).rounded()
+    archAngel.health -= ((joker.hattack) * (100/(100+archAngel.e2defense))).rounded()
 }
 
-func eAttack () {
+func pixieAttack () {
     joker.health -= ((pixie.e1attack) * (100/(100+joker.hdefense))).rounded()
+}
+
+func archAngelAttack () {
+    joker.health -= ((archAngel.e2attack) * (100/(100+joker.hdefense))).rounded()
 }
 
 func rakunda () {
 joker.sp -= 12
 if pixie.e1defense > 50 && joker.sp > 10  {
     pixie.e1defense = (pixie.e1defense / 2)
+    archAngel.e2defense = (archAngel.e2defense / 2)
     rakundaCount = 5
 }
 }
@@ -61,8 +68,9 @@ if pixie.e1defense > 50 && joker.sp > 10  {
 func rakundaDrop () {
     if rakundaCount <= 5 {
     rakundaCount -= 1
-        if rakundaCount == 0 && pixie.e1defense <= 50    {
+        if rakundaCount == 0 && pixie.e1defense <= 50 && archAngel.e2defense <= 100   {
         pixie.e1defense = (pixie.e1defense * 2)
+            archAngel.e2defense = (archAngel.e2defense * 2)
         print("enemy defenses return to normal")
 }
 }
@@ -133,7 +141,7 @@ difficultySelectLoop: while pixie.health >= 0 {
             }
             if joker.health > 0 {
                 rakundaDrop()
-                eAttack()
+                pixieAttack()
                 print("Joker has \(joker.health)HP remaining")
             }
             
@@ -150,7 +158,9 @@ difficultySelectLoop: while pixie.health >= 0 {
                 let playAgain = readLine()!.lowercased()
                 switch playAgain {
                 case "y":
+                    joker = (hattack: 50.0, hdefense: 50.0, health: 3000.0, sp: 50.0)
                     pixie = (e1attack: 60.0, e1defense: 100.0, health: 100.0)
+                    archAngel = (e2attack: 70.0, e2defense: 200.0, health: 100.0)
                     continue difficultySelectLoop
                 case "n" :
                     exit(32)
@@ -174,7 +184,7 @@ difficultySelectLoop: while pixie.health >= 0 {
                     let input = readLine()?.lowercased() ?? "attack"
                     switch input {
                     case "attack".lowercased():
-                        if pixie.health > 0 {
+                        if archAngel.health > 0 {
                             attack()
                             print("ArchAngel has \(archAngel.health)HP remaining")
                             }
@@ -221,7 +231,7 @@ difficultySelectLoop: while pixie.health >= 0 {
                     }
                     if joker.health > 0 {
                         rakundaDrop()
-                        eAttack()
+                        archAngelAttack()
                         print("Joker has \(joker.health)HP remaining")
                     }
                     
@@ -233,35 +243,27 @@ difficultySelectLoop: while pixie.health >= 0 {
                     if archAngel.health <= 0 {
                         print("You are victorious")
                         sleep(1)
-                        endingLoop: while archAngel.health <= 0 {
-                            print("You beat the hardest difficulty! Enter your name!")
-                            var name = readLine()!
+                        print("You beat the hardest difficulty!")
+                endingLoop: while archAngel.health <= 0 {
+                     print ("Enter your name!")
+                            let name = readLine()!
                             sleep(1)
-                            print("Enter your age!")
-                            var age = readLine()!
+                     print("Enter your age!")
+                    let age = readLine()!
+                    if let ageForced = Int(age) {
+                        
                             sleep(1)
+                         
+                            for _ in 1...3 {
+                                print("Hurrah! \(name) age: \(ageForced)")
                             
-                            for cheer in 1...3 {
-                                print("Hurrah! \(name) age: \(age)")
                             }
-                        let playAgain = readLine()!.lowercased()
-                        switch playAgain {
-                        case "y":
-                          archAngel = (e2attack: 70.0, e2defense: 200.0, health: 100.0)
-                            continue difficultySelectLoop
-                        case "n" :
-                            exit(32)
-                        default :
-                            continue endingLoop
+                        exit(32)
                         }
                     }
                 }
             }
-                
-            
-            
-            
-            
+        
             default:
             print("I don't know what you mean")
             
